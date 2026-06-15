8c45efd1c917efba007265895bdf530d16e001fd
LucifersCircle <31424518+LucifersCircle@users.noreply.github.com>
2026-06-15 00:58:16 -0700

    fix(generic): increase bufferInterval
    - was still easily able to hit 429 with buffer interval at 2. tried a few and i think this is as fast as you can go without any issue
    - version bump

--- src/generic/config.ts
+++ src/generic/config.ts
@@ -8,7 +8,7 @@ import {
   type SourceDeveloper,
 } from "@paperback/types";
 
-const BASE_VERSION = "1.0.0-alpha.10";
+const BASE_VERSION = "1.0.0-alpha.11";
 
 export const basePbConfig = {
   name: "",
